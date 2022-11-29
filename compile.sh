d9e99f6ce0ad39005e4605653c8c2ee28c7523a5
fm16191 <56755856+fm16191@users.noreply.github.com>
2022-11-29 17:47:20 +0100

    Update compile script.
    - Remove env commands as they should be performed by default in shell
      init
    - Script must be sourced.

--- compile.sh
+++ compile.sh
@@ -1,16 +1,20 @@
-source /etc/profile.d/modules.sh
-module use /opt/nvidia/hpc_sdk/modulefiles
-module use /opt/tools-hackathon/arm/compilers/modulefiles
-module use /opt/tools-hackathon/arm/forge/modulefiles
-module use /opt/tools-hackathon/openmpi/modulefiles
+#!/bin/bash
 
-export DEST=$HOME/code_saturne_7.2.0
+if [[ -n $ZSH_VERSION && $ZSH_EVAL_CONTEXT =~ :file$ ]] || 
+  [[ -n $BASH_VERSION ]] && (return 0 2>/dev/null)
+then
+    :
+else
+    echo "Usage : source $0" && exit 0
+fi
+
+export DEST="$HOME/code_saturne_7.2.0"
+export CC="mpicc"
+export CXX="mpic++"
+export FC="mpifort"
+
 mkdir -p build && cd build
-export CC=mpicc
-export CXX=mpicxx
-export FC=mpifort
-
-../configure CC="$CC" CXX="$CXX" FC="$FC" --prefix=$DEST --disable-gui --without-med --without-hdf5 --without-cgns--without-metis--disable-salome --without-salome --without-eos --disable-static --enable-long-gnum
+../configure CC="$CC" CXX="$CXX" FC="$FC" --prefix="$DEST" --disable-gui --without-med --without-hdf5 --without-cgns --without-metis --disable-salome --without-salome --without-eos --disable-static --enable-long-gnum
 
 make -j7
 make install
